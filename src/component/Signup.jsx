cd5b0b6807650c84866fb216cb63e107e2f3a479
Aarthi-aptonworks <aarthi@aptonworks.com>
2024-05-06 12:36:43 +0530

    changes

--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Signup = () => {
-  return <div>sdhflksjk</div>;
+  return (
+    <div>
+      fflkd
+      <p>fgkj</p>
+    </div>
+  );
 };
 
 export default Signup;
